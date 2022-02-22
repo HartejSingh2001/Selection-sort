45944c092e65fd5f4ba5c2e5567de8486d483582
HartejSingh2001 <86732914+HartejSingh2001@users.noreply.github.com>
2022-02-22 13:57:38 +0530

    Create ss.cpp

---
+++ ss.cpp
@@ -0,0 +1,28 @@
+// int select(int arr[], int i)
+// {
+//     // code here such that selecionSort() sorts arr[]
+       // No need to write anything here
+// }
+
+
+void selectionSort(int arr[], int n)
+{
+  int i,j,min,temp;
+  for(i=0;i<n-1;i++)
+  {
+      min=i;
+      for(j=i+1;j<n;j++)
+      {
+          if(arr[j]<arr[min])
+          {
+              min=j;
+          }
+      }
+      if(min!=i)
+      {
+          temp=arr[min];
+          arr[min]=arr[i];
+          arr[i]=temp;
+      }
+  }
+}
