c666c467586055fae97a7d0db15ac2687665c42a
Hartej Singh <86732914+HartejSingh2001@users.noreply.github.com>
2022-07-23 19:26:48 +0530

    Update ss.cpp

--- ss.cpp
+++ ss.cpp
@@ -1,10 +1,25 @@
+// Best - Apni Kaksha
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[i])
+            {
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+
+// End 
+
+
 // int select(int arr[], int i)
 // {
 //     // code here such that selecionSort() sorts arr[]
        // No need to write anything here
 // }
-
-
 void selectionSort(int arr[], int n)
 {
   int i,j,min,temp;
